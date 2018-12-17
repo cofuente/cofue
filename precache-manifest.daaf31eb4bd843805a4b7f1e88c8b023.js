5ad4708319992549e2b0821538c46bf7babb35ff
Chiara Marcial Martínez <cmarmar@gmail.com>
2018-12-17 17:07:48 -0500

    Updates

---
+++ precache-manifest.daaf31eb4bd843805a4b7f1e88c8b023.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "409abdd9daf377b4d4492b80247af0ad",
+    "url": "/static/media/cwd.409abdd9.pdf"
+  },
+  {
+    "revision": "033ee89bc10fe8b9f99772a07ed0a834",
+    "url": "/static/media/card.033ee89b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c8561fbaaf7a76f0972",
+    "url": "/static/js/main.8c8561fb.chunk.js"
+  },
+  {
+    "revision": "b9e000b179e658240231",
+    "url": "/static/js/1.b9e000b1.chunk.js"
+  },
+  {
+    "revision": "8c8561fbaaf7a76f0972",
+    "url": "/static/css/main.c7787793.chunk.css"
+  },
+  {
+    "revision": "5a25fcc8a2e862886e9f741d0cad869a",
+    "url": "/index.html"
+  }
+];
