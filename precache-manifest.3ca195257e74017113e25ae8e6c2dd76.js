7c9702609d9275bc581e8efaaf7343e434ba1264
cofuente <cmarmar@gmail.com>
2019-07-23 19:30:56 -0400

    Updates

---
+++ precache-manifest.3ca195257e74017113e25ae8e6c2dd76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43d0ed401e4f2e9cd6d56cdc2af28d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc288f36ef2a75ab73cc",
+    "url": "/static/css/main.f04fed95.chunk.css"
+  },
+  {
+    "revision": "cdbcde48b7bdb7664b68",
+    "url": "/static/js/2.fb4010c0.chunk.js"
+  },
+  {
+    "revision": "bc288f36ef2a75ab73cc",
+    "url": "/static/js/main.18aaa0ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "033ee89bc10fe8b9f99772a07ed0a834",
+    "url": "/static/media/card.033ee89b.jpg"
+  },
+  {
+    "revision": "409abdd9daf377b4d4492b80247af0ad",
+    "url": "/static/media/cwd.409abdd9.pdf"
+  }
+]);
