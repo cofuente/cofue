4b5094e08da16da4451519244b9e27d3aedc67ef
cofuente <cmarmar@gmail.com>
2019-07-23 20:51:44 -0400

    simplified temporarily

--- src/App.js
+++ src/App.js
@@ -6,23 +6,16 @@ class App extends Component {
 	render() {
 		return (
 			<div id="super">
-				<div className="css-typing" id="intro">
+				{/* <div className="css-typing" id="intro">
 					<h1>Hello, stranger.</h1>
 					<h1>Welcome to my page.</h1>
 					<h1>In all likelihood, I gave you the link myself.</h1>
 					<h1>So here, take my card.</h1>
 					<h1> -Go on, take it.</h1>
-				</div>
-				<div className="css-typing2" id="mobile-intro">
-					<h1>Hello, stranger.</h1>
-					<h1>Welcome to my page.</h1>
-					<h1>In all likelihood, I</h1>
-					<h1>gave you the link myself.</h1>
-					<h1>You probably want my card, huh?</h1>
-				</div>
+				</div> */}
 				<MyRBC />
-			<div id="rotate-device">please rotate your device<br/>🔄</div>
-			<div id="screen-too-small">They say size doesn't matter but your screen is legit too tiny to see this page accordingly.</div>
+			{/* <div id="rotate-device">please rotate your device<br/>🔄</div>
+			<div id="screen-too-small">They say size doesn't matter but your screen is legit too tiny to see this page accordingly.</div> */}
 		</div>)
 	}
 }
