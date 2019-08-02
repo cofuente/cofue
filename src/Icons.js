364461db393ce2a890624e3bbbea2de56103aadc
cofuente <cmarmar@gmail.com>
2019-08-01 21:22:50 -0400

    removed unused email icon

--- src/Icons.js
+++ src/Icons.js
@@ -1,31 +1,24 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAt } from '@fortawesome/free-solid-svg-icons'
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faFilePdf } from '@fortawesome/free-regular-svg-icons'
 
-export class EmailIcon extends Component {
-	render() {
-		return <FontAwesomeIcon icon={faAt} size="2x" aria-hidden={true} />
-	}
+export class GithubIcon extends PureComponent {
+  render() {
+    return (
+      <FontAwesomeIcon icon={faGithubSquare} size='2x' aria-hidden />
+    )
+  }
 }
 
-export class GithubIcon extends Component {
-	render() {
-		return (
-			<FontAwesomeIcon icon={faGithubSquare} size="2x" aria-hidden={true} />
-		)
-	}
+export class LinkedinIcon extends PureComponent {
+  render() {
+    return <FontAwesomeIcon icon={faLinkedin} size='2x' aria-hidden />
+  }
 }
 
-export class LinkedinIcon extends Component {
-	render() {
-		return <FontAwesomeIcon icon={faLinkedin} size="2x" aria-hidden={true} />
-	}
-}
-
-export class ResumeIcon extends Component {
-	render() {
-		return <FontAwesomeIcon icon={faFilePdf} size="2x" aria-hidden={true} />
-	}
+export class ResumeIcon extends PureComponent {
+  render() {
+    return <FontAwesomeIcon icon={faFilePdf} size='2x' aria-hidden />
+  }
 }
