073a7a103d3b845bbc98eb49bff3f892601a6936
cofuente <cmarmar@gmail.com>
2019-08-01 21:22:03 -0400

    moved dimension logic into App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
+import { MyRBC } from './MyRBC'
+import { TextConsole } from './TextConsole'
 import './App.css'
-import MyRBC from './MyRBC'
 
-class App extends Component {
-	render() {
-		return (
-			<div id="super">
-				<div className="css-typing" id="intro">
-					<h1>Hello, stranger.</h1>
-					<h1>Welcome to my page.</h1>
-					<h1>In all likelihood, I gave you the link myself.</h1>
-					<h1>So here, take my card.</h1>
-					<h1> -Go on, take it.</h1>
-				</div>
-				<MyRBC />
-		</div>)
-	}
+export default class App extends PureComponent {
+  render() {
+    const windowHeight = window.innerHeight
+    const windowWidth = window.innerWidth
+    const orientation = windowWidth > windowHeight ? 'landscape' : 'portrait'
+    const aspectRatio = windowWidth / windowHeight
+    let cardHeight
+    let cardWidth
+    if (orientation === 'portrait') {
+      cardWidth = windowWidth < 750 ? windowWidth * 0.9 : 700
+      cardHeight = cardWidth < 700 ? ((cardWidth * 4) / 7) : 400
+    }
+    if (orientation === 'landscape') {
+      if (windowHeight >= 700) {
+        cardWidth = 700
+        cardHeight = 400
+      } else if (aspectRatio >= 1.75) {
+        cardHeight = windowHeight * 0.9
+        cardWidth = cardHeight * 1.75
+      } else {
+        cardWidth = windowWidth * 0.9
+        cardHeight = (cardWidth * 4) / 7
+      }
+    }
+    return (
+      <div id='super'>
+        <TextConsole />
+        <MyRBC width={cardWidth} height={cardHeight} />
+      </div>
+    )
+  }
 }
-
-export default App
--- src/MyRBC.js
+++ src/MyRBC.js
@@ -1,39 +1,20 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import ReactBusinessCard from './ReactBusinessCard'
 import cardStock from './card.png'
 import CardContent from './CardContent'
-class MyRBC extends Component {
-	render() {
-		const windowHeight = window.innerHeight
-		const windowWidth = window.innerWidth
-		const orientation = windowWidth > windowHeight ? 'landscape' : 'portrait'
-		const aspectRatio = windowWidth/windowHeight
-		let cardHeight, cardWidth
-		if (orientation==="portrait") {
-			cardWidth = windowWidth < 750 ? windowWidth * .9 : 700
-			cardHeight = cardWidth < 700 ? ((cardWidth * 4)/7) : 400
-		}
-		if (orientation==="landscape") {
-			if (windowHeight >= 700 ) {
-				cardWidth = 700
-				cardHeight = 400
-			} else if (aspectRatio >= 1.75 ) {
-				cardHeight = windowHeight * 0.9
-				cardWidth = cardHeight * 1.75
-			} else {
-				cardWidth = windowWidth * 0.9
-				cardHeight = (cardWidth * 4)/7
-			}
-		}
-		return (
-			<div id="rbc">
-				<ReactBusinessCard width={cardWidth} height={cardHeight}>
-					<img ref="image" width={cardWidth} height={cardHeight} src={cardStock} alt="The lovely card stock I picked for this printing. Like it?"/>
-					<CardContent width={cardWidth} height={cardHeight} />
-				</ReactBusinessCard>
-			</div>
-		)
-	}
+
+export class MyRBC extends PureComponent {
+  render() {
+    const { width, height } = this.props
+    return (
+      <div id='rbc'>
+        <ReactBusinessCard width={width} height={height}>
+          <img width={width} height={height} src={cardStock} alt='The lovely card stock I picked for this printing. Like it?' />
+          <CardContent width={width} height={height} />
+        </ReactBusinessCard>
+      </div>
+    )
+  }
 }
 
 export default MyRBC
