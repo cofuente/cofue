06affb852590a7022ba41e99c91bc37b3731b37a
cofuente <cmarmar@gmail.com>
2019-08-01 21:22:42 -0400

    moved console

---
+++ src/TextConsole.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react'
+
+export class TextConsole extends PureComponent {
+  render() {
+    return (
+      <div className='css-typing' id='intro'>
+        <h1>Hello, stranger.</h1>
+        <h1>Welcome to my page.</h1>
+        <h1>In all likelihood, I gave you the link myself.</h1>
+        <h1>So here, take my card.</h1>
+        <h1> -Go on, take it.</h1>
+      </div>
+    )
+  }
+}
+
+export default TextConsole
