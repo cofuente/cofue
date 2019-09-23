4d41296e0b55476a5a8bdbe29c158b527b983067
cofuente <cmarmar@gmail.com>
2019-09-23 18:42:06 -0400

    truncated message, altered timing, altered spacing

--- src/TextConsole.js
+++ src/TextConsole.js
@@ -6,15 +6,25 @@ export class TextConsole extends PureComponent {
   render() {
     return (
       <div id='console-container'>
-        <Typing className='console' speed={60}>
+        <Typing className='console' speed={35}>
           <div className='line' id='line-1'>
-            <span>Hey there.</span>
-            <Typing.Delay ms={600} />
-            <br />
+            <span>Hey stranger.</span>
+            <Typing.Delay ms={900} />
           </div>
-          <div className='line' id='line-5'>
-            <Typing.Speed ms={70} />
-            <span>Take my card.</span>
+          <div className='line' id='line-2'>
+            <br />
+            <span>Why don&apos;t you take my card?</span>
+          </div>
+          <div className='line' id='line-3'>
+            <br />
+            <span>-</span>
+            <span role='img' aria-label='Alien'>&#128126;</span>
+            <Typing.Speed ms={120} />
+            <Typing.Delay ms={375} />
+            <span role='img' aria-label='Seedling'>&#127793;</span>
+            <Typing.Delay ms={375} />
+            <span role='img' aria-label='Red Heart'>&#10084;&#65039;</span>
+            <Typing.Delay ms={120000} />
           </div>
         </Typing>
       </div>
