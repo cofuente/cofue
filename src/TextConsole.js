2a5da89ea9b9a107c0012e7b1f3a2a801822e73d
cofuente <cmarmar@gmail.com>
2019-09-23 16:18:47 -0400

    removed MOST of intro

--- src/TextConsole.js
+++ src/TextConsole.js
@@ -8,33 +8,12 @@ export class TextConsole extends PureComponent {
       <div id='console-container'>
         <Typing className='console' speed={60}>
           <div className='line' id='line-1'>
-            <span>Hey there, stranger.</span>
-            <Typing.Delay ms={600} />
-            <br />
-          </div>
-          <div className='line' id='line-2'>
-            <Typing.Speed ms={70} />
-            <span>How&apos;s life? </span>
-            <span role='img' aria-label='Seedling'>&#127793;</span>
-            <Typing.Delay ms={1000} />
-            <br />
-          </div>
-          <div className='line' id='line-3'>
-            <span>Doing well, I ho</span>
-            <Typing.Speed ms={600} />
-            <span>pe...</span>
-            <Typing.Delay ms={600} />
-            <Typing.Speed ms={60} />
-            <br />
-          </div>
-          <div className='line' id='line-4'>
-            <span>Wanna actually tell me about it?</span>
+            <span>Hey there.</span>
             <Typing.Delay ms={600} />
             <br />
           </div>
           <div className='line' id='line-5'>
             <Typing.Speed ms={70} />
-            <span>Cool! </span>
             <span>Take my card.</span>
           </div>
         </Typing>
