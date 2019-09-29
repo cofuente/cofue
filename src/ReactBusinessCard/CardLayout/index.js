1a92657b9b81f29ca8155cf48860c6eb478811da
cofuente <cmarmar@gmail.com>
2019-09-29 18:03:02 -0400

    moved Icons.js into CardLayout

--- src/ReactBusinessCard/CardLayout/index.js
+++ src/ReactBusinessCard/CardLayout/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { GithubIcon, LinkedinIcon, ResumeIcon } from '../Icons'
+import { GithubIcon, LinkedinIcon, ResumeIcon } from './Icons'
 import './styles.css'
 
 const downloadResume = (uri, first, last) => {
